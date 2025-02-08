efc16034af84a537317531a5015d1290bc3c9a84
massifrg <massifrg@gmail.com>
2025-02-08 23:40:58 +0100

    fix: styling for gapcursor

--- packages/renderer/src/PundokApp.vue
+++ packages/renderer/src/PundokApp.vue
@@ -599,5 +599,35 @@ $color-fg-index: #dc7200;
   img.ProseMirror-selectednode {
     border: 2px solid #3584e4;
   }
+
+  // start of gapcursor.css
+
+  .ProseMirror-gapcursor {
+    display: none;
+    pointer-events: none;
+    position: absolute;
+  }
+
+  .ProseMirror-gapcursor:after {
+    content: "";
+    display: block;
+    position: absolute;
+    top: -2px;
+    width: 20px;
+    border-top: 1px solid black;
+    animation: ProseMirror-cursor-blink 1.1s steps(2, start) infinite;
+  }
+
+  @keyframes ProseMirror-cursor-blink {
+    to {
+      visibility: hidden;
+    }
+  }
+
+  .ProseMirror-focused .ProseMirror-gapcursor {
+    display: block;
+  }
+
+  // end of gapcursor.css
 }
 </style>
