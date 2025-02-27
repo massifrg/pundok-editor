9e7936349562917191294b560801db0633264a8f
massifrg <massifrg@gmail.com>
2025-02-27 18:25:45 +0100

    fix: RawTexEditor supports multiline texts

--- packages/renderer/src/components/attreditors/RawTextEditor.vue
+++ packages/renderer/src/components/attreditors/RawTextEditor.vue
@@ -1,13 +1,24 @@
 <template>
   <div class="q-pa-sm">
-    <q-input v-model="text" filled dark outlined type="textarea" autofocus
-      input-style="font-family: monospace; color: antiquewhite" class="text-h6" spellcheck="false" @keydown="keydown" />
+    <q-input v-model="text" filled dark outlined type="textarea" autofocus :input-style="inputStyle()" class="text-h6"
+      spellcheck="false" @keydown="keydown" />
   </div>
 </template>
 
 <script lang="ts">
+const INPUT_COMMON_STYLES = [
+  "font-family: monospace",
+  "color: antiquewhite",
+  "line-height: 1.4",
+  "font-size: smaller",
+  "min-width: 70vw",
+]
+const INPUT_IS_BLOCK_STYLES = [
+  "min-height: 70vh",
+]
+
 export default {
-  emits: ['update-attribute', 'enter-key-pressed', 'esc-key-pressed'],
+  emits: ['update-attribute', 'commit', 'cancel'],
   props: ['startValue', 'format', 'isBlock'],
   data() {
     return {
@@ -29,15 +40,24 @@ export default {
     keydown(e: KeyboardEvent) {
       switch (e.key) {
         case 'Enter':
-          if (!this.isBlock) e.preventDefault()
-          this.$emit('enter-key-pressed')
+          if (this.isBlock && !e.ctrlKey)
+            return true
+          else
+            e.preventDefault()
+          this.$emit('commit')
           break
         case 'Escape':
-          this.$emit('esc-key-pressed')
+          this.$emit('cancel')
           e.preventDefault()
           break
       }
     },
+    inputStyle() {
+      return [
+        ...INPUT_COMMON_STYLES,
+        ...(this.isBlock ? INPUT_IS_BLOCK_STYLES : [])
+      ].join(';')
+    }
   }
 }
 </script>
