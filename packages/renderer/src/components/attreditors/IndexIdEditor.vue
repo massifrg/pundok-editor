ae96f8601dd6dce36bb8d4d8421bfc4908046d4e
massifrg <massifrg@gmail.com>
2025-11-12 10:52:21 +0100

    fix: don't repeat search until project indices refresh has finished

--- packages/renderer/src/components/attreditors/IndexIdEditor.vue
+++ packages/renderer/src/components/attreditors/IndexIdEditor.vue
@@ -237,7 +237,7 @@ export default {
           this.doSearch()
           break
         case 'project':
-          this.refreshProjectIndices()
+          await this.refreshProjectIndices()
           this.doSearch()
           break
         default:
