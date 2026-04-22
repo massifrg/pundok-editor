ba2c09364b38ec3619ed4508fcd22dc05b2c106e
massifrg <massifrg@gmail.com>
2026-04-22 16:15:15 +0200

    fix: SpanMarkView.vue replaced with a better vanilla js implementation

--- packages/renderer/src/schema/marks/Span.ts
+++ packages/renderer/src/schema/marks/Span.ts
@@ -1,8 +1,6 @@
 import { Mark, mergeAttributes } from '@tiptap/core';
-import { VueMarkViewRenderer } from '@tiptap/vue-3';
-import { MARK_NAME_SPAN, SK } from '../../common';
-import { getSpanAttrs } from '../helpers';
-import { SpanMarkView } from '../../components';
+import { appliesTo, MARK_NAME_SPAN, SK } from '../../common';
+import { getDocState, getSpanAttrs } from '../helpers';
 
 export interface SpanOptions {
   HTMLAttributes: Record<string, any>;
@@ -48,7 +46,49 @@ export const Span = Mark.create<SpanOptions>({
   },
 
   addMarkView() {
-    return VueMarkViewRenderer(SpanMarkView)
+    // return VueMarkViewRenderer(SpanMarkView)
+    return ({ mark, HTMLAttributes, view }) => {
+      const dom = document.createElement('span')
+      const docState = getDocState(view.state)
+      const configuration = docState?.configuration
+      let customStyle: string | undefined = undefined
+      if (configuration) {
+        const attrs = mark.attrs
+        const { id, classes, kv } = attrs
+        if (id)
+          dom.setAttribute("id", id)
+        customStyle = kv && kv['custom-style']
+        if (customStyle) {
+          const styleDef = configuration?.customStyles?.find(
+            (d) => d.name == customStyle && appliesTo(d, MARK_NAME_SPAN)
+          )
+          if (styleDef && Array.isArray(styleDef.css) && styleDef.css.length > 0) {
+            dom.setAttribute("style", styleDef.css
+              .map(([prop, value]) => `${prop}: ${value}`)
+              .join('; '))
+          }
+        }
+        Object.entries({ ...HTMLAttributes, ...kv }).forEach(([k, v]) => {
+          if (k === 'custom-style' || k === 'idref')
+            dom.setAttribute(k, v as string)
+          else
+            dom.setAttribute('data-' + k, v as string)
+        })
+        const isCustomized = !!customStyle
+          || !!configuration.customClasses?.find((cc) => !!(classes as string[]).find(c => c === cc.name))
+        dom.setAttribute("class", [
+          ...classes,
+          isCustomized ? 'custom-style' : 'unstyled-custom-style'
+        ].join(' '))
+      }
+
+      const contentDOM = document.createElement('span')
+      dom.appendChild(contentDOM)
+      return {
+        dom,
+        contentDOM,
+      }
+    }
   },
 
   addCommands() {
